ddd9a14e5663417cb2325fbc297e54fd0fbe4cb3
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-02-16 20:51:02 +0300

    Changed p0081viewbyid

--- p0081viewbyid/src/main/java/com/course/p0081viewbyid/MainActivity.java
+++ p0081viewbyid/src/main/java/com/course/p0081viewbyid/MainActivity.java
@@ -2,6 +2,10 @@ package com.course.p0081viewbyid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myTextView = (TextView) findViewById(R.id.myText);
+        myTextView.setText("New text in TextView");
+
+        Button myBtn = (Button) findViewById(R.id.myBtn);
+        myBtn.setText("My button");
+        myBtn.setEnabled(false);
+
+        CheckBox myChb = (CheckBox) findViewById(R.id.myChb);
+        myChb.setChecked(true);
     }
 }
