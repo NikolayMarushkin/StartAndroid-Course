4f9e88dc741b1491dded41a613cfc0efbd5fb6ce
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-02-17 16:12:17 +0300

    Changed p0131menusimple

--- p0131menusimple/src/main/java/com/course/p0131menusimple/MainActivity.java
+++ p0131menusimple/src/main/java/com/course/p0131menusimple/MainActivity.java
@@ -2,6 +2,10 @@ package com.course.p0131menusimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+
+        menu.add("menu1");
+        menu.addSubMenu("addSubMenu1");
+        menu.add("menu2");
+        menu.add("menu3");
+        menu.add("menu4");
+        menu.addSubMenu("addSubMenu2").add("1");
+        menu.addSubMenu("addSubMenu3").add("2");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
