7d3c01c05c19f671957bb6d4ecc85e026a36a0d9
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-02-17 15:27:17 +0300

    Changed p0121logandmess

--- p0121logandmess/src/main/java/com/course/p0121logandmess/MainActivity.java
+++ p0121logandmess/src/main/java/com/course/p0121logandmess/MainActivity.java
@@ -1,13 +1,59 @@
 package com.course.p0121logandmess;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    private static final String TAG = "MyLogs";
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        Log.d(TAG, "найдем View-элементы");
+        Toast.makeText(this, "найдем View-элементы", Toast.LENGTH_LONG).show();
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // присваиваем обработчик кнопкам
+        Log.d(TAG, "присваиваем обработчик кнопкам");
+        Toast.makeText(this, "присваиваем обработчик кнопкам", Toast.LENGTH_LONG).show();
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        // по id определяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                Log.d(TAG, "кнопка ОК");
+                Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                Log.d(TAG, "кнопка Cancel");
+                Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
 }
