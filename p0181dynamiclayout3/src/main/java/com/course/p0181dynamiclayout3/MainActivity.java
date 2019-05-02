be2ecb2ce87b313a4a5d1a00f7d70798d5f5f973
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-05-02 11:21:44 +0300

    Add p0181dynamiclayout3

---
+++ p0181dynamiclayout3/src/main/java/com/course/p0181dynamiclayout3/MainActivity.java
@@ -0,0 +1,58 @@
+package com.course.p0181dynamiclayout3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    SeekBar sbWeight;
+    Button btn1;
+    Button btn2;
+
+    LinearLayout.LayoutParams lParams1;
+    LinearLayout.LayoutParams lParams2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sbWeight = findViewById(R.id.sbWeight);
+        sbWeight.setOnSeekBarChangeListener(this);
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        lParams1 = (LinearLayout.LayoutParams) btn1.getLayoutParams();
+        lParams2 = (LinearLayout.LayoutParams) btn2.getLayoutParams();
+
+    }
+
+    // onProgressChanged срабатывает все время, пока значение меняется
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int leftValue = progress;
+        int rightValue = seekBar.getMax() - progress;
+        // настраиваем вес
+        lParams1.weight = leftValue;
+        lParams2.weight = rightValue;
+        // в текст кнопок пишем значения переменных
+        btn1.setText(String.valueOf(leftValue));
+        btn2.setText(String.valueOf(rightValue));
+    }
+
+    // onStartTrackingTouch срабатывает, когда начинаем тащить ползунок
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    // onStopTrackingTouch срабатывает, когда отпускаем ползунок
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+}
