55d790ee4500c17c63c7277f06b965f59c60dcc3
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-05-03 18:46:01 +0300

    Add p0241twoactivitystate

--- p0241twoactivitystate/src/main/java/com/course/p0241twoactivitystate/MainActivity.java
+++ p0241twoactivitystate/src/main/java/com/course/p0241twoactivitystate/MainActivity.java
@@ -1,17 +1,31 @@
-package com.course.p0231oneactivitystate;
+package com.course.p0241twoactivitystate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnActTwo = findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -42,5 +56,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
     }
 }
