f47250ad4deed18ffde2a66ef739d5df9f978b8f
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-05-04 11:59:29 +0300

    Add p0291simpleactivityresult

---
+++ p0291simpleactivityresult/src/main/java/com/course/p0291simpleactivityresult/MainActivity.java
@@ -0,0 +1,41 @@
+package com.course.p0291simpleactivityresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvName;
+    Button btnName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvName = findViewById(R.id.tvName);
+        btnName = findViewById(R.id.btnName);
+        btnName.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, NameActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            Toast.makeText(this, "Некорректные данные", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String name = data.getStringExtra("name");
+        tvName.setText("Your name is " + name);
+    }
+}
