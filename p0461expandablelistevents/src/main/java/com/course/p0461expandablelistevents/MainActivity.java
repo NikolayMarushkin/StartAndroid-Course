6bc32e078198c9ca1a4f5eef2d697894005c2d8b
NikolayMarushkin <nikolaymarushkin94@gmail.com>
2019-05-09 09:23:21 +0300

    Add p0461expandablelistevents

--- p0461expandablelistevents/src/main/java/com/course/p0461expandablelistevents/MainActivity.java
+++ p0461expandablelistevents/src/main/java/com/course/p0461expandablelistevents/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // разворачиваем группу с позицией 2
+        // разворачиваем группу с позицией: 2
         elvMain.expandGroup(1);
     }
 }
